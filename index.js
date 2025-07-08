61041998a85af00d008077183244fca86aba523a
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-07-09 08:54:25 +0900

    portrait support

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const viewer = document.querySelector('.viewer');
-const wrapper = viewer.querySelector('.image-wrapper');
+document.querySelectorAll(".viewer").forEach(viewer => {
+  const wrapper = viewer.querySelector('.image-wrapper');
+  const img = wrapper.querySelector('img');
 
-function setTilt(x, y, bounds) {
-  const centerX = bounds.width / 2;
-  const centerY = bounds.height / 2;
-  const rotateX = -(y - centerY) / 20;
-  const rotateY = (x - centerX) / 20;
-  wrapper.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-}
+  img.onload = () => {
+    const aspectRatio = img.naturalWidth / img.naturalHeight;
 
-viewer.addEventListener('mousemove', (e) => {
-  const bounds = viewer.getBoundingClientRect();
-  setTilt(e.clientX - bounds.left, e.clientY - bounds.top, bounds);
-});
+    if (aspectRatio < 1) {
+      // Portrait
+      viewer.style.aspectRatio = '3 / 4';
+      img.style.objectFit = 'contain'; // Show entire image
+    } else {
+      // Landscape
+      viewer.style.aspectRatio = '4 / 3';
+      img.style.objectFit = 'cover'; // Fill space
+    }
+  };
 
-viewer.addEventListener('mouseleave', () => {
-  wrapper.style.transform = `rotateX(0deg) rotateY(0deg)`;
-});
+  // 3D mouse/touch interaction
+  function setTilt(x, y, bounds) {
+    const centerX = bounds.width / 2;
+    const centerY = bounds.height / 2;
+    const rotateX = -(y - centerY) / 20;
+    const rotateY = (x - centerX) / 20;
+    wrapper.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  }
 
-viewer.addEventListener('touchmove', (e) => {
-  const touch = e.touches[0];
-  const bounds = viewer.getBoundingClientRect();
-  setTilt(touch.clientX - bounds.left, touch.clientY - bounds.top, bounds);
-});
+  viewer.addEventListener('mousemove', (e) => {
+    const bounds = viewer.getBoundingClientRect();
+    setTilt(e.clientX - bounds.left, e.clientY - bounds.top, bounds);
+  });
 
-viewer.addEventListener('touchend', () => {
-  wrapper.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  viewer.addEventListener('mouseleave', () => {
+    wrapper.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  });
+
+  viewer.addEventListener('touchmove', (e) => {
+    const touch = e.touches[0];
+    const bounds = viewer.getBoundingClientRect();
+    setTilt(touch.clientX - bounds.left, touch.clientY - bounds.top, bounds);
+  });
+
+  viewer.addEventListener('touchend', () => {
+    wrapper.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  });
 });
